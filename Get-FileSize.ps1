312e2ab23bc1563099d892fa4605a57c9d0da88c
briancasey1 <33161953+briancasey1@users.noreply.github.com>
2018-04-04 10:12:33 -0400

    Create Get-FileSize.ps1
    Quick function to convert bytes to a human-friendly file size.

---
+++ Get-FileSize.ps1
@@ -0,0 +1,20 @@
+function Get-FileSize {
+    [CmdletBinding()]
+    param(
+        # Input file size in bytes
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [int64]$Bytes
+    )
+
+    process {
+        switch ($Bytes) {
+            {$Bytes -gt 1tb} {$Result = "$([math]::Round(($Bytes / 1tb),2)) TB"; break}
+            {$Bytes -gt 1gb} {$Result = "$([math]::Round(($Bytes / 1gb),2)) GB"; break}
+            {$Bytes -gt 1mb} {$Result = "$([math]::Round(($Bytes / 1mb),2)) MB"; break}
+            {$Bytes -gt 1kb} {$Result = "$([math]::Round(($Bytes / 1kb),2)) KB"; break}
+            default {$Result = "$($Bytes) B"}
+        }
+
+        return $Result
+    }
+}
